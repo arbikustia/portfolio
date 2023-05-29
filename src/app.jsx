e08687e3c38dbd303bde547e146ba260516d18bb
arbi kustia <arbikustia14@gmail.com>
2023-05-29 09:15:31 +0700

    handle router

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,17 @@
 
-import Root from './routes/root'
-import Skills from './routes/skills'
-import Projects from './routes/projects'
-import Contact from './routes/contact'
+import Root from "./routes/root"
+import Skills from "./routes/skills"
+// import Projects from "./routes/projects"
+// import Contact from "./routes/contact"
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Root />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        <Routes>
+            <Route path="/" element={<Root/>} />
+            <Route path="/skills" element={<Skills/>} />
+        </Routes>
     </BrowserRouter>
   );
 }
